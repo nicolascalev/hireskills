aa977076184f1bb3220fd9e8d0b2113a13f8704c
nicolascalev <nicolascalevg@gmail.com>
2024-03-02 21:01:20 -0500

    Add state for project filters, load them from url search params, update url when filters change listen to changes in the parent component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import ProjectCard from "@/app/ui/ProjectCard";
 import { Container, Grid, GridCol, Stack } from "@mantine/core";
 import ProjectFilters from "../ui/ProjectFilters";
 import ProjectFiltersDrawer from "../ui/ProjectFiltersDrawer";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function ProjectsPage() {
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    console.log("from parent", searchParams.toString());
+  }, [searchParams]);
+
   return (
     <Container py="md" size="xl">
       <Grid gutter={{ base: "md", sm: "xl" }}>
--- app/ui/ProjectFilters.tsx
+++ app/ui/ProjectFilters.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Checkbox,
   CheckboxGroup,
@@ -6,43 +7,175 @@ import {
   Switch,
   TextInput,
 } from "@mantine/core";
+import { useDebouncedValue } from "@mantine/hooks";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useEffect, useMemo, useState } from "react";
+import SkillsCombobox from "./SkillsCombobox";
+import ToolsCombobox from "./ToolsCombobox";
+
+const SORT_OPTIONS = {
+  "Popular top": '{"likeCount": "desc"}',
+  "Least popular top": '{"likeCount": "asc"}',
+  "Newest top": '{"createdAt": "desc"}',
+  "Oldest top": '{"createdAt": "asc"}',
+};
+const DEFAULT_SORT = SORT_OPTIONS["Popular top"];
+const SORT_INPUT_OPTIONS = Object.entries(SORT_OPTIONS).map(
+  ([label, value]) => ({
+    label,
+    value,
+  })
+);
+function isValidSortOrDefault(value: string | null) {
+  if (!value) return DEFAULT_SORT;
+  return value in SORT_OPTIONS ? value : DEFAULT_SORT;
+}
+const DEFAULT_LEVEL = ["basic", "intermediate", "advanced"];
 
 function ProjectFilters() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [sort, setSort] = useState(
+    isValidSortOrDefault(searchParams.get("sort"))
+  );
+  const [search, setSearch] = useState(searchParams.get("search") || "");
+  const [debouncedSearch] = useDebouncedValue(search, 500);
+  const [level, setLevel] = useState(
+    searchParams.getAll("level").length == 0
+      ? DEFAULT_LEVEL
+      : searchParams.getAll("level")
+  );
+  const [visibleSkills, setVisibleSkills] = useState<string[]>(
+    searchParams.getAll("skills")
+  );
+  const [skills, setSkills] = useState<string[]>(searchParams.getAll("skills"));
+  const [visibleTools, setVisibleTools] = useState<string[]>(
+    searchParams.getAll("tools")
+  );
+  const [tools, setTools] = useState<string[]>(searchParams.getAll("tools"));
+  // next do used by people and verified projects only
+  const [isUsedByPeople, setIsUsedByPeople] = useState(
+    searchParams.get("isUsedByPeople") === "true"
+  );
+  const [isVerified, setIsVerified] = useState(
+    searchParams.get("isVerified") === "true"
+  );
+
+  const projectFilters = useMemo(() => {
+    const filters = new URLSearchParams();
+    filters.set("sort", sort);
+    filters.set("search", debouncedSearch);
+    filters.set("isUsedByPeople", JSON.stringify(isUsedByPeople));
+    filters.set("isVerified", JSON.stringify(isVerified));
+    level.forEach((value) => filters.append("level", value));
+    skills.forEach((value) => filters.append("skills", value));
+    tools.forEach((value) => filters.append("tools", value));
+    return filters;
+  }, [sort, debouncedSearch, level, isUsedByPeople, isVerified, skills, tools]);
+
+  useEffect(() => {
+    replace(`${pathname}?${projectFilters.toString()}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [projectFilters]);
+
   return (
     <>
       <Select
         label="Sort"
-        defaultValue="Popular top"
-        data={["Popular top", "Least popular top", "Newest top", "Oldest top"]}
+        data={SORT_INPUT_OPTIONS}
+        value={sort}
+        onChange={(value) => {
+          setSort(value || DEFAULT_SORT);
+        }}
       />
-      <TextInput label="Search" placeholder="Search" />
+      <TextInput
+        label="Search"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.currentTarget.value)}
+      />
       <CheckboxGroup
-        defaultValue={["Basic", "Intermediate", "Advanced"]}
         label="Project level"
+        value={level}
+        onChange={(value) => {
+          setLevel(value);
+        }}
       >
         <Stack gap="5px" mt="xs">
-          <Checkbox value="Basic" label="Basic" color="dark" />
-          <Checkbox value="Intermediate" label="Intermediate" color="dark" />
-          <Checkbox value="Advanced" label="Advanced" color="dark" />
+          <Checkbox value="basic" label="Basic" color="teal" />
+          <Checkbox value="intermediate" label="Intermediate" color="teal" />
+          <Checkbox value="advanced" label="Advanced" color="teal" />
         </Stack>
       </CheckboxGroup>
-      <TextInput label="Skills" placeholder="Skills" />
-      <TextInput label="Tools" placeholder="Tools" />
+
+      <Stack gap="xs">
+        <SkillsCombobox
+          label="Skills"
+          value={visibleSkills}
+          onChange={(newItem) => {
+            setVisibleSkills([newItem, ...visibleSkills]);
+            setSkills([newItem, ...skills]);
+          }}
+        />
+        {visibleSkills.length > 0 && (
+          <CheckboxGroup value={skills} onChange={setSkills}>
+            <Stack gap="xs">
+              {visibleSkills.map((skill) => (
+                <Checkbox
+                  label={skill}
+                  value={skill}
+                  key={skill}
+                  color="teal"
+                />
+              ))}
+            </Stack>
+          </CheckboxGroup>
+        )}
+      </Stack>
+
+      <Stack gap="xs">
+        <ToolsCombobox
+          label="Tools"
+          value={visibleTools}
+          onChange={(newItem) => {
+            setVisibleTools([newItem, ...visibleTools]);
+            setTools([newItem, ...tools]);
+          }}
+        />
+        {visibleTools.length > 0 && (
+          <CheckboxGroup value={tools} onChange={setTools}>
+            <Stack gap="xs">
+              {visibleTools.map((tool) => (
+                <Checkbox label={tool} value={tool} key={tool} color="teal" />
+              ))}
+            </Stack>
+          </CheckboxGroup>
+        )}
+      </Stack>
+
       <Switch
-        defaultChecked
         labelPosition="left"
         label="Used by people only"
-        color="dark"
+        color="teal"
         size="sm"
         styles={{ body: { justifyContent: "space-between" } }}
+        checked={isUsedByPeople}
+        onChange={(event) => {
+          setIsUsedByPeople(event.currentTarget.checked);
+        }}
       />
       <Switch
-        defaultChecked
         labelPosition="left"
         label="Verified projects only"
-        color="dark"
+        color="teal"
         size="sm"
         styles={{ body: { justifyContent: "space-between" } }}
+        checked={isVerified}
+        onChange={(event) => {
+          setIsVerified(event.currentTarget.checked);
+        }}
       />
     </>
   );
